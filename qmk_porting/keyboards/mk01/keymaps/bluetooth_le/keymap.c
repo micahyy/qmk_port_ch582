3000038056b7dcfb3da37287867c8707f18a71a2
Micah <45813290+micahyy@users.noreply.github.com>
2026-03-19 23:00:34 +0800

    5

--- qmk_porting/keyboards/mk01/keymaps/bluetooth_le/keymap.c
+++ qmk_porting/keyboards/mk01/keymaps/bluetooth_le/keymap.c
@@ -25,8 +25,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
                      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, LT(1, KC_LEFT), LT(2, KC_DOWN), RCTL_T(KC_RIGHT)),
     [1] = LAYOUT_all(/* 1: fn */
-                     KC_GRV, BLE_SLOT0, BLE_SLOT1, BLE_SLOT2, BLE_SLOT3, BLE_ALL_CLEAR, _______, _______, _______, _______, _______, _______, RGB_MOD, KC_DEL,
-                     _______, _______, KC_UP, _______, _______, _______, _______, _______, _______, RGB_HUD, RGB_HUI, _______, _______, _______,
+                     KC_GRV, BLE_SLOT0, BLE_SLOT1, BLE_SLOT2, BLE_SLOT3, BLE_ALL_CLEAR, _______, _______, _______, _______, _______, _______, RM_NEXT, KC_DEL,
+                     _______, _______, KC_UP, _______, _______, _______, _______, _______, _______, _______,_______, _______, _______, _______,
                      _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, RM_SATD, RM_SATU, _______, _______, RM_TOGG,
                      _______, _______, _______, _______, _______, _______, RM_VALD, RM_VALU, _______, _______, _______, _______,
                      _______, _______, _______, _______, _______, _______, _______, _______),
