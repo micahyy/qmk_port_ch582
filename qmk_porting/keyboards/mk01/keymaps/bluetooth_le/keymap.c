19546c495601b932d755650a627aa01cb4ed0752
Micah <45813290+micahyy@users.noreply.github.com>
2026-03-19 22:49:13 +0800

    3

--- qmk_porting/keyboards/mk01/keymaps/bluetooth_le/keymap.c
+++ qmk_porting/keyboards/mk01/keymaps/bluetooth_le/keymap.c
@@ -16,6 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
+#include "rgb_matrix.h"  // RGB 控制键码定义
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_all(/* 0: qwerty */
                      KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
@@ -24,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
                      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, LT(1, KC_LEFT), LT(2, KC_DOWN), RCTL_T(KC_RIGHT)),
     [1] = LAYOUT_all(/* 1: fn */
-                     KC_GRV, BLE_SLOT0, BLE_SLOT1, BLE_SLOT2, BLE_SLOT3, BLE_ALL_CLEAR, _______, _______, _______, _______, _______, RGB_MODE_REVERSE, RGB_MODE_FORWARD, KC_DELETE,
+                     KC_GRV, BLE_SLOT0, BLE_SLOT1, BLE_SLOT2, BLE_SLOT3, BLE_ALL_CLEAR, _______, _______, _______, _______, _______, RGB_RMOD, RGB_MOD, KC_DEL,
                      _______, _______, KC_UP, _______, _______, _______, _______, _______, _______, RGB_HUD, RGB_HUI, _______, _______, _______,
                      _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, RGB_SAD, RGB_SAI, _______, _______, RGB_TOG,
                      _______, _______, _______, _______, _______, _______, RGB_VAD, RGB_VAI, _______, _______, _______, _______,
