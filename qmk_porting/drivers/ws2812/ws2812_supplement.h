d6916e6842af08dd463155d13bd91a784bfeb612
Micah <45813290+micahyy@users.noreply.github.com>
2026-03-20 22:34:58 +0800

    33

--- qmk_porting/drivers/ws2812/ws2812_supplement.h
+++ qmk_porting/drivers/ws2812/ws2812_supplement.h
@@ -1,21 +1,5 @@
-/*
-Copyright 2024 Huckies <https://github.com/Huckies>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#pragma once
+#ifndef WS2812_SUPPLEMENT_H
+#define WS2812_SUPPLEMENT_H
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -25,5 +9,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define WS2812_TIMEOUT 100
 #endif
 
+// ========== WS2812 时序参数 ==========
+#ifndef WS2812_TIMING
+#define WS2812_TIMING 1250  // 1.25μs = 1250ns，WS2812 的位周期
+#endif
+
+#ifndef WS2812_TRST_US
+#define WS2812_TRST_US 50   // 复位时间 50μs（WS2812 要求 >50μs）
+#endif
+
 void ws2812_power_toggle(bool status);
-bool ws2812_power_get();
+bool ws2812_power_get(void);
+
+#endif
