0afa7b67014d57daf657ab3124a8a78a923b7201
Micah <45813290+micahyy@users.noreply.github.com>
2026-03-20 22:28:34 +0800

    5

--- qmk_porting/keyboards/mk01/qmk_config.h
+++ qmk_porting/keyboards/mk01/qmk_config.h
@@ -142,3 +142,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define NO_ACTION_LAYER
 // #define NO_ACTION_TAPPING
 // #define NO_ACTION_ONESHOT
+
+#define CONFIG_USBDEV_EP_NUM     8
+#define CONFIG_USBDEV_MAX_BUS    1
